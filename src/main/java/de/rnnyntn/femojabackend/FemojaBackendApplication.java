b06c23e6541fe2b2ce45ce9059f2e16b337909a9
rnnyntn <rnnyntn@gmail.com>
2022-01-14 23:56:10 +0100

    Initial commit

---
+++ src/main/java/de/rnnyntn/femojabackend/FemojaBackendApplication.java
@@ -0,0 +1,13 @@
+package de.rnnyntn.femojabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemojaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FemojaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/rnnyntn/femojabackend/FemojaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rnnyntn.femojabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemojaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
